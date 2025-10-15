c1135bc779dddf601b28af403942df042801d137
agustin herrera <agustinherrera.dev@gmail.com>
2025-10-15 13:36:06 -0300

    Add test seed values for Institues and Students

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,53 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[
+  'instituto secundario san miguel',
+  'instituto técnico regional',
+  'colegion secundario los álamos'
+].each do |institute_name|
+  Institute.create!(name: institute_name)
+end
+
+san_miguel = Institute.find(1)
+tecnico_regional = Institute.find(2)
+los_alamos = Institute.find(3)
+
+[
+  {
+    institute: san_miguel,
+    students: [
+      [ 'martín', 'lópez', 17 ],
+      [ 'camila', 'rodríguez', 18 ],
+      [ 'sofía', 'herrera', 16 ]
+    ]
+  },
+  {
+    institute: tecnico_regional,
+    students: [
+      [ 'maria', 'pérez', 18 ],
+      [ 'juan', 'gómez', 18 ],
+      [ 'diego', 'castillo', 17 ]
+    ]
+  },
+  {
+    institute: los_alamos,
+    students: [
+      [ 'mateo', 'morales', 16 ],
+      [ 'sebastián', 'torres', 17 ],
+      [ 'isabella', 'sánchez', 17 ]
+    ]
+  }
+].each do |institute_hash|
+  institute = institute_hash[:institute]
+  students = institute_hash[:students]
+
+  students.each do |first_name, last_name, age|
+    institute.students.create!(
+      first_name: first_name,
+      last_name: last_name,
+      age: age
+    )
+  end
+end

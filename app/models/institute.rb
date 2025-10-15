e109e255fabc164f31654e658a72ca107ba54ef3
agustin herrera <agustinherrera.dev@gmail.com>
2025-10-15 13:35:14 -0300

    Update Insitute model with has_many students association

--- app/models/institute.rb
+++ app/models/institute.rb
@@ -1,4 +1,6 @@
 class Institute < ApplicationRecord
+  has_many :students, dependent: :destroy
+
   validates :name, length: { maximum: 120 }, presence: true, uniqueness: true
   normalizes :name, with: ->(name) { name.strip.downcase }
 end

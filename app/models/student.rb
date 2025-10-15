ac40bfeda4b0d574a129d15efaaf6b7b86715784
agustin herrera <agustinherrera.dev@gmail.com>
2025-10-15 12:26:45 -0300

    Validate and normalize fields of Student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :institute
+  validates :first_name, :last_model, length: { maximum: 60 }, presence: true
+  validates :age, numericality: { only_integer: true, in: 16..22 }
+  normalizes :first_name, :last_model, with: ->(e) { e.strip.downcase }
 end

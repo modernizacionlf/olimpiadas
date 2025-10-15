501ddc9109a513f532301aea17071ed0ced7986b
agustin herrera <agustinherrera.dev@gmail.com>
2025-10-15 13:27:08 -0300

    Fix typo in Student last_name model field

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,9 @@
 class Student < ApplicationRecord
   belongs_to :institute
-  validates :first_name, :last_model, length: { maximum: 60 }, presence: true
+
+  validates :first_name, :last_name, length: { maximum: 60 }, presence: true
   validates :age, numericality: { only_integer: true, in: 16..22 }
-  normalizes :first_name, :last_model, with: ->(e) { e.strip.downcase }
+  normalizes :first_name, :last_name, with: ->(e) { e.strip.downcase }
 
   def full_name
     "#{first_name} #{last_name}"

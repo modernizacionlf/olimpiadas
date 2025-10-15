864b00f30f9b490305757b6f9defa64735d159ec
agustin herrera <agustinherrera.dev@gmail.com>
2025-10-15 12:27:58 -0300

    Add full_name Student model function

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,8 @@ class Student < ApplicationRecord
   validates :first_name, :last_model, length: { maximum: 60 }, presence: true
   validates :age, numericality: { only_integer: true, in: 16..22 }
   normalizes :first_name, :last_model, with: ->(e) { e.strip.downcase }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end

89d3b4ef1cca14976982e48a7236105a8b447b83
agustin herrera <agustinherrera.dev@gmail.com>
2025-10-15 13:34:29 -0300

    Update schema creating students table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_15_144455) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_15_151755) do
   create_table "institutes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_institutes_on_name", unique: true
   end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.integer "institute_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["institute_id"], name: "index_students_on_institute_id"
+  end
+
+  add_foreign_key "students", "institutes"
 end

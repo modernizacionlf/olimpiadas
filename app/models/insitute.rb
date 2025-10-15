42b3df81d9573a4f0c1288ba4de3021d95a70e7e
agustin herrera <agustinherrera.dev@gmail.com>
2025-10-15 11:48:09 -0300

    Add Institute model

---
+++ app/models/insitute.rb
@@ -0,0 +1,2 @@
+class Insitute < ApplicationRecord
+end
---
+++ test/models/insitute_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class InsituteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

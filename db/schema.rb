ec86fce1cc286fa7bebb647ddb632ae60206c17f
agusherrera99 <agustinherrera.dev@gmail.com>
2025-10-16 09:54:43 -0300

    Add places table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_16_120006) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_16_124908) do
   create_table "institutes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_institutes_on_name", unique: true
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_places_on_name", unique: true
   end
 
   create_table "students", force: :cascade do |t|

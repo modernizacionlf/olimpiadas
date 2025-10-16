8225be329750561fd840cfaed8c6dcbdf0c009dd
agusherrera99 <agustinherrera.dev@gmail.com>
2025-10-16 09:54:06 -0300

    Ensure database uniqueness constraint of name field for Places table

--- db/migrate/20251016124908_create_places.rb
+++ db/migrate/20251016124908_create_places.rb
@@ -1,9 +1,10 @@
 class CreatePlaces < ActiveRecord::Migration[8.0]
   def change
     create_table :places do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
+    add_index :places, :name,  unique: true
   end
 end

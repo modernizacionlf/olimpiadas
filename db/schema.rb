ff1339d8e48f3596a8e3f22e8cfbe6b55b54f536
agusherrera99 <agustinherrera.dev@gmail.com>
2025-10-28 10:48:50 -0300

    Update schema with images table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_28_133852) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_28_133942) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_28_133852) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "institutes", force: :cascade do |t|

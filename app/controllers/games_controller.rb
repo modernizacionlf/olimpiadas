d6ccfd582485a7fb4bc594944070fced512780bc
agusherrera99 <agustinherrera.dev@gmail.com>
2025-10-29 10:47:35 -0300

    Remove class instances of Games index and show action controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,10 +2,8 @@ class GamesController < ApplicationController
   allow_unauthenticated_access only: %i[index show]
 
   def index
-    @games = Games.all
   end
 
   def show
-    @game = Game.find(params[:id])
   end
 end

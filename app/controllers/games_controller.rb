a7b71aa7d14a0af9186a6f231d684038f0bdcdf4
agusherrera99 <agustinherrera.dev@gmail.com>
2025-11-03 10:23:32 -0300

    delete index action of GamesController

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,5 @@
 class GamesController < ApplicationController
-  allow_unauthenticated_access only: %i[index show]
-
-  def index
-    @games = Game.all
-  end
+  allow_unauthenticated_access only: %i[show]
 
   def show
   end

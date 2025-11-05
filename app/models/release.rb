8dcf1fb5b137043d0a9c1c8bfddab635f009a9a8
agusherrera99 <agustinherrera.dev@gmail.com>
2025-11-05 08:52:43 -0300

    Update maximum length of description for cultural and release models

--- app/models/release.rb
+++ app/models/release.rb
@@ -2,6 +2,7 @@ class Release < ApplicationRecord
   has_one_attached :image
 
   validates :title, length: { minimum: 10, maximum: 80 }, presence: true
+  validates :description, length: { minimum: 10, maximum: 500 }, presence: true
 
   normalizes :title, :description, with: -> (e) { e.strip.downcase }
 end

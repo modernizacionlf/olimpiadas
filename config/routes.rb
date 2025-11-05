429a2a714fbc3868239aa7a7ba3a3f54ea17c481
agusherrera99 <agustinherrera.dev@gmail.com>
2025-11-05 08:49:26 -0300

    Add cultural routing

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :releases, only: %i[index show]
   resources :gallery, only: %i[index new create]
   resources :sports, only: %i[index show]
+  resources :cultural, only: %i[index show]
 
   namespace :admin do
     resources :releases

10c302931a045697aeb23594498d2fbd8d287392
agusherrera99 <agustinherrera.dev@gmail.com>
2025-11-05 11:38:33 -0300

    Add promo model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_05_114625) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_05_143321) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -77,6 +77,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_05_114625) do
     t.index ["name"], name: "index_places_on_name", unique: true
   end
 
+  create_table "promos", force: :cascade do |t|
+    t.integer "institute_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["institute_id"], name: "index_promos_on_institute_id"
+  end
+
   create_table "releases", force: :cascade do |t|
     t.string "title", null: false
     t.string "description", null: false
@@ -124,6 +131,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_05_114625) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "games", "game_instances"
   add_foreign_key "games", "sports"
+  add_foreign_key "promos", "institutes"
   add_foreign_key "sessions", "users"
   add_foreign_key "students", "institutes"
 end

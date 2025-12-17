085d0ec0e3caf5c04523cad15dc0f4235ae1fa08
agusherrera99 <agustinherrera.dev@gmail.com>
2025-12-17 08:44:57 -0300

    Extend cultural and releases title and description maximum validation

--- app/models/cultural.rb
+++ app/models/cultural.rb
@@ -1,8 +1,8 @@
 class Cultural < ApplicationRecord
   has_one_attached :image
 
-  validates :title, length: { minimum: 10, maximum: 80 }, presence: true
-  validates :description, length: { minimum: 10, maximum: 500 }, presence: true
+  validates :title, length: { minimum: 10, maximum: 150 }, presence: true
+  validates :description, length: { minimum: 10, maximum: 4000 }, presence: true
 
   normalizes :title, :description, with: ->(e) { e.strip.downcase }
 end
--- app/models/release.rb
+++ app/models/release.rb
@@ -2,7 +2,7 @@ class Release < ApplicationRecord
   has_one_attached :image
 
   validates :title, length: { minimum: 10, maximum: 150 }, presence: true
-  validates :description, length: { minimum: 10, maximum: 500 }, presence: true
+  validates :description, length: { minimum: 10, maximum: 4000 }, presence: true
 
   normalizes :title, :description, with: -> (e) { e.strip.downcase }
 end

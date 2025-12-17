686849d81e8b5abdcecac2595cef69ce9d54b8fe
agusherrera99 <agustinherrera.dev@gmail.com>
2025-12-17 08:42:51 -0300

    Add gallery controller photos class instance variable

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -2,5 +2,8 @@ class GalleryController < ApplicationController
   allow_unauthenticated_access only: %i[index]
   
   def index
+    @photos = ActiveStorage::Attachment.joins(:blob)
+      .where.not(record_type: 'Promo')
+      .where("active_storage_blobs.content_type LIKE ?", "image/%")
   end
 end
